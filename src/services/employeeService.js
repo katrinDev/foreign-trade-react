fb9614bd5a67536f7447390f52ed40e1f54d91c5
katrinDev <polozkovak8@gmail.com>
2023-05-13 08:05:38 +0300

    create employee api service

---
+++ src/services/employeeService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/employees";
+
+class EmployeeService {
+  getEmployees() {
+    return axios.get(API_URL);
+  }
+
+  addEmployee(employee) {
+    return axios.post(API_URL, employee);
+  }
+
+  updateEmployee(employee) {
+    return axios.put(API_URL, employee);
+  }
+
+  deleteEmployee(id) {
+    return axios.delete(API_URL + `/${id}`);
+  }
+}
+
+export default new EmployeeService();
